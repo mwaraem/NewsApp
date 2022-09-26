d44e66303c2bd44a8b085cfc56a0a235deda0cd5
mwaraem <mwaraem@gmail.com>
2022-09-26 20:07:49 +0300

    "Initial commit"

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
@@ -1,6 +1,7 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
+import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
 
-class SavedNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 }
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "76c08ac64ac444a0ae941da80ee953e2"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
